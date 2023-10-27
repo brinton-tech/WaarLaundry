c208d9789525ff87929a930390e71c7d69df48d7
brinton-tech <121550679+brinton-tech@users.noreply.github.com>
2023-10-27 11:46:52 +0300

    Add files via upload

---
+++ Laundry_Management_System-master/contact.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/style.css">
+<center>
+    <h1><pre><b>                                          Contact Us                         
+                                                       Chiranjib Parida
+                                                                cell: +91 7788924623
+                    mail: chiranjibparida99@gmail.com
+</b></pre></h1>
+</br>
+<h1><b>Return To Home</b></h1>
+<div class="btn" onclick="location.href='./home.php';">WAAR Cleaners</div>
+</center>
